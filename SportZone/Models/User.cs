5d908ff70fe79683acb02d48c15e43bf25acfbf3
Tim Van Dorpe <tim.van.dorpe@hotmail.com>
2025-10-13 21:25:14 +0200

    feat: Add User model with MongoDB attributes

---
+++ SportZone/Models/User.cs
@@ -0,0 +1,32 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SportZone.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("email")]
+    [BsonRequired]
+    public string Email { get; set; } = string.Empty;
+
+    [BsonElement("password")]
+    [BsonRequired]
+    public string Password { get; set; } = string.Empty;
+
+    [BsonElement("name")]
+    [BsonRequired]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("preferredSport")]
+    public string? PreferredSport { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
