263aa67cbb7b722fdda243c3d13d63d6bcffd606
Tim Van Dorpe <tim.van.dorpe@hotmail.com>
2025-10-13 21:28:38 +0200

    feat: Configure dependency injection and MongoDB in Program.cs

--- SportZone/Program.cs
+++ SportZone/Program.cs
@@ -1,10 +1,35 @@
+using SportZone.Configuration;
+using SportZone.Repositories;
+using SportZone.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Configure MongoDB settings
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+// Register repositories
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
+// Register services
+builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -15,6 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
