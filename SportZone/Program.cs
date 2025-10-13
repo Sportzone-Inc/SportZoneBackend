0fa0d4bf4a22b41ceccbd9641900d5ab60ca4eab
Tim Van Dorpe <tim.van.dorpe@hotmail.com>
2025-10-13 21:44:22 +0200

    feat: Configure Swagger UI with SportActivity services

--- SportZone/Program.cs
+++ SportZone/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using SportZone.Configuration;
 using SportZone.Repositories;
 using SportZone.Services;
@@ -13,13 +15,34 @@ builder.Services.Configure<MongoDbSettings>(
 
 // Register repositories
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<ISportActivityRepository, SportActivityRepository>();
 
 // Register services
 builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISportActivityService, SportActivityService>();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "SportZone API",
+        Description = "Een ASP.NET Core Web API voor het beheren van sportactiviteiten en gebruikers",
+        Contact = new OpenApiContact
+        {
+            Name = "SportZone Team",
+            Email = "info@sportzone.be"
+        }
+    });
+
+    // Gebruik XML commentaren voor Swagger documentatie
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    options.IncludeXmlComments(xmlPath);
+});
 
 // Add CORS
 builder.Services.AddCors(options =>
@@ -36,7 +59,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "SportZone API v1");
+        options.RoutePrefix = string.Empty; // Swagger UI op root URL
+    });
 }
 
 app.UseHttpsRedirection();
