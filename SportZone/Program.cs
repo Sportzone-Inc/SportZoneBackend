0f91070d828a673f436dd753cede2f2d090c4ff5
Tim Van Dorpe <tim.van.dorpe@hotmail.com>
2025-10-27 21:08:09 +0100

    Configure JSON serializer to use string values for enums in Swagger and API responses

--- SportZone/Program.cs
+++ SportZone/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Text;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -12,7 +13,12 @@ using SportZone.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON serializer to use string values for enums
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 // Configure MongoDB settings
 builder.Services.Configure<MongoDbSettings>(
